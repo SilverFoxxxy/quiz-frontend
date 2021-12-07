04544865cfc506f42acd0b12f0c7a9965ce16803
SilverFoxxxy <ivan-belkov2@yandex.ru>
2021-12-07 05:42:14 +0300

    upd

--- test_http.js
+++ test_http.js
@@ -2,7 +2,7 @@
 // const server = 'https://45.143.93.51/';
 // const server = 'http://45.143.93.51/';
 // const server = 'localhost:8080/';
-const server = 'https://letters-reader2.tk/quiz/';
+const server = 'https://letters-reader2.tk/query2/';
 
 async function send_req(req_json) {
     console.log(JSON.stringify(req_json));
@@ -20,6 +20,7 @@ async function send_req(req_json) {
     if (response.ok) { // если HTTP-статус в диапазоне 200-299
       // получаем тело ответа (см. про этот метод ниже)
       let res_json = await response.json();
+      console.log(res_json);
       return res_json;
     } else {
         // alert("Ошибка HTTP: " + response.status);
@@ -45,7 +46,7 @@ function calc_hash(str) {
     return digestMessage(str + salt);
 }
 
-async function add_round(round_json) {
+async function req_add_round(round_json) {
     // calc_hash(pswd);
     let req_json = round_json;
     req_json["type"] = "add_round";
@@ -53,7 +54,17 @@ async function add_round(round_json) {
     return res_json;
 }
 
-async function get_round_list() {
+async function req_del_round(round_id) {
+    // calc_hash(pswd);
+    let req_json = {
+      type: "del_round",
+      round_id: round_id
+    }
+    let res_json = send_req(req_json);
+    return res_json;
+}
+
+async function req_get_round_list() {
     let req_json = {
         type: "get_round_list"
     };
@@ -61,7 +72,7 @@ async function get_round_list() {
     return res_json;
 }
 
-async function get_question_list(round_id) {
+async function req_get_question_list(round_id) {
     let req_json = {
         type: "get_question_list",
         round_id: round_id
@@ -70,13 +81,11 @@ async function get_question_list(round_id) {
     return res_json;
 }
 
-async function get_rating_table() {
+async function req_get_rating_table() {
     let req_json = {
         type: "get_rating_table"
     };
     let res_json = send_req(req_json);
     return res_json;
 }
-
-
 
--- test_page.js
+++ test_page.js
@@ -1,5 +1,25 @@
 
 async function show_rounds() {
-    let nowjson = await get_round_list();
+    let nowjson = await req_get_round_list();
     document.getElementById('round_list').innerHTML = JSON.stringify(nowjson, null, '\t');
+}
+
+async function show_questions() {
+    let round_id = parseInt(document.getElementById('round_id_1').value);
+    let nowjson = await req_get_question_list(round_id);
+    document.getElementById('question_list').innerHTML = JSON.stringify(nowjson, null, '\t');
+}
+
+async function add_round() {
+    let strjson = document.getElementById('round_info').value;
+    let nowjson = JSON.parse(strjson);
+    console.log(nowjson);
+    let res = await req_add_round(nowjson);
+    document.getElementById('add_round_res').innerHTML = JSON.stringify(res, null, '\t');
+}
+
+async function del_round() {
+    let round_id = parseInt(document.getElementById('del_round_id').value);
+    let res = await req_del_round(round_id);
+    document.getElementById('del_round_res').innerHTML = JSON.stringify(res, null, '\t');
 }
